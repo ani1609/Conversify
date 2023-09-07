d9507aafbd7d9a77f9e740e381f4a40e0a03baca
Ankit Kr Chowdhury <89239354+ani1609@users.noreply.github.com>
2023-09-07 19:29:19 +0530

    basic user authentication added

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import '../styles/App.css';
+import Navbar from '../components/Navbar.js';
+import Chat from './Chat.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      {/* <Chat/> */}
+    </div>
+  );
+}
+
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,22 +1,71 @@
 const express=require("express");
-const app=express();
+const connectDb = require('./configDB/MongoDB');
+const {login, signup, authenticateJWT, changePassword}=require('./controllers/userController');
 const http=require('http');
 const{ Server }=require('socket.io');
 const cors=require('cors');
+
+const app=express();
 const port=3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+connectDb();
 
 const server=http.createServer(app);
 
-const io=new  Server(server,{
-    cors: {
-        origin: "http://localhost:3001",
-        methods: ["GET", "POST"],
-    },
+
+// --------user controllers--------
+app.post('/api/users/login', login);
+app.post('/api/users/signup', signup);
+app.get('/api/user', authenticateJWT, (req, res) => 
+{
+    res.json({ message: 'Protected route accessed successfully!', user: req.user });
 });
+
+
+// const io=new  Server(server,{
+//     cors: {
+//         origin: "http://localhost:3001",
+//         methods: ["GET", "POST"],
+//     },
+// });
+
+// io.on('connection', (socket) => 
+// {
+//     console.log('A user connected');
+
+//     // Handle create room
+//     socket.on('create_room', (roomId) => 
+//     {
+//         socket.join(roomId);
+//         console.log(`User created and joined a room: ${roomId}`);
+//     });
+
+//     // Handle joining room
+//     socket.on('join_room', (roomId) => 
+//     {
+//         socket.join(roomId);
+//         console.log(`User joined a room: ${roomId}`);
+//     });
+
+//     //handle send message
+//     socket.on('send_message', (data) => 
+//     {
+//         console.log("received message ", data);
+//         io.to(data.roomId).emit('receive_message', { message: data.message });
+//     });
+
+//     socket.on('disconnect', () => 
+//     {
+//         console.log('A user disconnected');
+//     });
+// });
 
 server.listen(port, () =>
 {
     console.log(`server is listening on port ${port}`);
 });
+
