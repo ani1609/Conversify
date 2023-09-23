e26bd810df35371cd7a19648c0ce4f95398b5bc7
Ankit Kr Chowdhury <89239354+ani1609@users.noreply.github.com>
2023-09-24 01:31:02 +0530

    theme enhancement

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -56,16 +56,16 @@ function Navbar()
 
 
     return(
-        <div className={dark ? 'navbar_parent dark_primary-bg' : 'navbar_parent light_primary-bg'} style={{ borderBottom: dark ? '1px solid rgb(78, 78, 78)' : '1px solid rgb(165, 165, 165)' }}>
-            <a href='' className={dark ? 'dark_primary' : 'light_primary'}>Conversify</a>
+        <div className={dark ? 'navbar_parent dark_bg' : 'navbar_parent light_bg'} style={{ borderBottom: dark ? '1px solid rgb(78, 78, 78)' : '1px solid rgb(165, 165, 165)' }}>
+            <a href=''>Conversify</a>
             <ul className='nav_tabs'>
-                <li onClick={()=>setDark(!dark)} className={dark ? 'theme dark_primary-hover dark_primary-border' : 'theme light_primary-hover light_primary-border'}>
+                <li onClick={()=>setDark(!dark)} className={dark ? 'theme dark_hover dark_border' : 'theme light_hover light_border'}>
                         {dark ? <Dark className='dark_icon'/> : <Light className='light_icon'/>}
                 </li>
-                {!userToken && <li onClick={()=>setShowLoginForm(true)} className={dark ? 'login dark_primary-hover dark_primary-border dark_primary' : 'login light_primary-hover light_primary-border light_primary'}>Log in</li>}
-                {!userToken && <li onClick={()=>setShowSignupForm(true)} className={dark ? 'signup dark_primary-hover dark_primary-border dark_primary' : 'signup light_primary-hover light_primary-border light_primary'}>Sign up</li>}
+                {!userToken && <li onClick={()=>setShowLoginForm(true)} className={dark ? 'login dark_hover dark_border' : 'login light_hover light_border'}>Log in</li>}
+                {!userToken && <li onClick={()=>setShowSignupForm(true)} className={dark ? 'signup dark_hover dark_border' : 'signup light_hover light_border'}>Sign up</li>}
                 {userToken && user?.profilePic && <li className='profile_pic_wrapper'><img src={user.profilePic} alt='profile_pic'/></li>}
-                {userToken && !user.profilePic && <li className={dark ? 'profile_icon_wrapper dark_primary-hover dark_primary-border dark_fill' : 'profile_icon_wrapper light_primary-hover light_primary-border light_fill'} onMouseEnter={() => setProfileDropDown(true)}><Profile className='profile_icon'/></li>}
+                {userToken && !user.profilePic && <li className={dark ? 'profile_icon_wrapper dark_hover dark_border' : 'profile_icon_wrapper light_hover light_border'} onMouseEnter={() => setProfileDropDown(true)}><Profile className='profile_icon'/></li>}
             </ul>
             {profileDropDown &&
                 <ul className='profile_dropdown' onMouseEnter={() => setProfileDropDown(true)} onMouseLeave={() => setProfileDropDown(true)}>
